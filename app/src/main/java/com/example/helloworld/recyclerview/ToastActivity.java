bf31cb71115d6be0b88758506e07d9c67e384910
xxy <1452769581@qq.com>
2023-03-31 14:54:55 +0800

    woshidameinv

--- app/src/main/java/com/example/helloworld/recyclerview/ToastActivity.java
+++ app/src/main/java/com/example/helloworld/recyclerview/ToastActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.helloworld.MainActivity;
 import com.example.helloworld.R;
@@ -13,7 +15,7 @@ import com.example.helloworld.TextViewActivity;
 
 public class ToastActivity extends AppCompatActivity {
 
-    private Button mBtnToast1,mBtnToast2,mBtnToast3;
+    private Button mBtnToast1,mBtnToast2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +23,21 @@ public class ToastActivity extends AppCompatActivity {
         setContentView(R.layout.activity_toast);
         mBtnToast1 = findViewById(R.id.btn_Toast_1);
         mBtnToast2 = findViewById(R.id.btn_Toast_2);
-        mBtnToast3 = findViewById(R.id.btn_Toast_3);
         OnClick onClick = new OnClick();
         mBtnToast1.setOnClickListener(onClick);
         mBtnToast2.setOnClickListener(onClick);
-        mBtnToast3.setOnClickListener(onClick);
     }
     class OnClick implements View.OnClickListener{
         @Override
         public void onClick(View v){
-            switch (v.getID()){
+            switch (v.getId()){
                 case R.id.btn_Toast_1:
+                    Toast.makeText(getApplicationContext(), "Toast", Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.btn_Toast_2:
-                    break;
-                case R.id.btn_Toast_3:
+                    Toast toastCenter = Toast.makeText(getApplicationContext(), "居中Toast", Toast.LENGTH_SHORT);
+                    toastCenter.setGravity(Gravity.CENTER,0,0);
+                    toastCenter.show();
                     break;
             }
         }
